6ac5ec42ef2400d019e240e5bf6c30b2aa656e77
Kamila Gajcy <kamila.gajcy@gmail.com>
2021-04-28 13:31:46 +0200

    Add init OrderForm and processOrder methods

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       console.log('new Product:', thisProduct);
     }
@@ -105,6 +107,32 @@
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
+    initOrderForm(){
+      const thisProduct = this;
+      console.log(thisProduct); //informujący o nazwie metody w której się znajduje??
+    
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;
+      console.log(this);
+    }
+
   }
   const app = {
     initMenu: function(){
