b0bc8f9b2280074cff1b0a473ac6a3df20a84937
Kamila Gajcy <kamila.gajcy@gmail.com>
2021-05-04 22:27:44 +0200

    Change processOrder method to calculate price

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,7 @@
     all: {
       menuProducts: '#product-list > .product',
       menuProductsActive: '#product-list > .product.active',
-      formInputs: 'input, select',
+      formInputs: 'input, select',//z tego powstaje ten selector select.all.formInputs, skad pochodzą te wartosci 'input, select'
     },
     menuProduct: {
       clickable: '.product__header',
@@ -54,7 +54,7 @@
   class Product{
     constructor(id, data){
       const thisProduct = this;
-      thisProduct.id = id;
+      thisProduct.id = id; 
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
@@ -68,13 +68,9 @@
     
     renderInMenu(){
       const thisProduct = this;
-      /* generate HTML based on template */
       const generatedHTML = templates.menuProduct(thisProduct.data);
-      /* create element using utils.createElementFromHTML */
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-      /* find menu container */
       const menuContainer = document.querySelector(select.containerOf.menu);
-      /* add element DOM to the found menu container*/
       menuContainer.appendChild(thisProduct.element);
     }
 
@@ -82,34 +78,42 @@
       const thisProduct = this;
     
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log(thisProduct.accordionTrigger);
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      console.log(thisProduct.form);
+
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      console.log(thisProduct.formInputs);
+      //ciezko mi zrozumiec ten selector, w console.log wyswietla sie NodeList?nie wiem co to jest
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      console.log(thisProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      console.log(thisProduct.priceElem);
     }
   
-    initAccordion() {
+    initAccordion(){
       const thisProduct = this;
+      
       
       /* find the clickable trigger (the element that should react to clicking)
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable) START: add event listener to clickable trigger on event click */
       thisProduct.accordionTrigger.addEventListener('click', function(event) {
-        /* prevent default action for event */
         event.preventDefault();
-        /* find active product (product that has active class) */
         const activeProduct = document.querySelector(select.all.menuProductsActive);
+        console.log(this);
+        console.log(thisProduct);
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct !== null && activeProduct !== thisProduct.element){
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }  
-        /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
+
     initOrderForm(){
       const thisProduct = this;
-      console.log(thisProduct); //informujący o nazwie metody w której się znajduje??
+      console.log('initOrderForm'); //informujący o nazwie metody w której się znajduje??
     
       thisProduct.form.addEventListener('submit', function(event){
         event.preventDefault();
@@ -128,12 +132,58 @@
       });
     }
 
-    processOrder(){
+    processOrder() {
       const thisProduct = this;
-      console.log(this);
-    }
 
+      console.log('processOrder'); //pokazuje sie w konsoli kiedy klikne add to cart
+    
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+    
+      // set price to default price
+      let price = thisProduct.data.price;
+    
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+        console.log(paramId);
+        console.log (param);
+    
+        // for every option in this category
+        for(let optionId in param.options) {// nazwa stałej.klucz obiektu ??
+          
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+
+          const option = param.options[optionId];
+        
+          console.log(option);
+          console.log(param.options);
+          console.log(optionId);
+          
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          // check if the option is not default
+            if(option.default !== true) {
+              // add option price to price variable
+              price+=option.price;
+            }
+          } else {
+          // check if the option is default
+            if(option.default == true) {
+              // reduce price variable
+              price-=option.price;
+            }
+          }
+        }
+        // update calculated price in the HTML
+        thisProduct.priceElem.innerHTML = price;
+      }
+    }
   }
+
   const app = {
     initMenu: function(){
       const thisApp = this;
@@ -154,6 +204,7 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
       thisApp.initData();
       thisApp.initMenu();
     },
